f1b1353eff54dc6afe9b7ff2359cd978505bd102
EricLee <851212656@qq.com>
2017-06-22 10:55:56 +0800

    2017/06/22

---
+++ d0621/h0917.py
@@ -0,0 +1,17 @@
+import math
+from math import sqrt
+import cmath
+
+if 1 == 2 : print('One equals two')
+if 1 == 1 : print('One equals one')
+2 ** 3
+x =  pow(2,3)
+print(x)
+y = math.floor(x + 0.5)
+print(y)
+z = sqrt(9)
+print(z)
+q = cmath.sqrt(-1)
+print(q)
+w = (1+3j) * (9+4j)
+print(w)
