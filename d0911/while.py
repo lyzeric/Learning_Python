fbca297807759ce129efdbf1dfd0db8b9366694c
YizhuoLee <851212656@qq.com>
2017-09-12 22:01:44 +0800

    update 2017/09/12

---
+++ d0911/while.py
@@ -0,0 +1,14 @@
+number = 23
+running = True
+while running:
+    guess = int(input('Enter an integer:'))
+    if guess == number:
+        print('Congratulation, you guessed it.')
+        running = False
+    elif guess < number:
+        print('No, it is a little higher than that.')
+    else:
+        print('No, it is a little lower than that.')
+else:
+    print('The while loop is over.')
+print('Done')
