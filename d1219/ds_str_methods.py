9c5072ba77a272e4cc2d57dd9a3065481a0b2c15
YizhuoLee <851212656@qq.com>
2017-12-20 09:52:04 +0800

    update 2017/12/20

---
+++ d1219/ds_str_methods.py
@@ -0,0 +1,13 @@
+name = 'Swaroop'
+if name.startswith('Swa'):
+    print('Yes, the string starts with "Swa"')
+
+if 'a' in name:
+    print('Yes, it contains the string "a"')
+
+if name.find('war') != -1:
+    print('Yes, it contains the string "war"')
+
+delimiter = '_*_'
+mylist = ['Brazil', 'Russian', 'India', 'China']
+print(delimiter.join(mylist))
